d8b4882fb12c0d6a4c798fc70e0ad06255f134ae
Matvieiev Kyrylo <kyrylo.matvieiev@gmail.com>
2018-05-11 08:22:58 +0300

    Create async function ”fight”

--- src/fight.js
+++ src/fight.js
@@ -1,1 +1,23 @@
 // create async function fight
+
+async function fight(fighter, improvedFighter, points) {
+    while(fighter.health >= 0 && improvedFighter.health >= 0) {
+        fighter.hit(improvedFighter, points);
+        improvedFighter.doubleHit(fighter, points);
+    }
+    if(fighter.health <= 0) {
+        console.log("user in knockout");
+        let promise = fighter.knockout();
+        promise
+            .then(
+                result => {console.log(improvedFighter.name + " win")}
+            );
+    } else if(improvedFighter.health <= 0) {
+        console.log("user in knockout");
+        let promise = improvedFighter.knockout();
+        promise
+            .then(
+                result => {console.log(fighter.name + " win")}
+            );
+    }
+}
