f7e2659bc153de0414865d85290380b6ab18d8e1
Matvieiev Kyrylo <kyrylo.matvieiev@gmail.com>
2018-05-11 08:32:51 +0300

    Game Start
    Writed function fight(fighter, improvedFighter, [25, 13, 45]);

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@
 var fighter = new Fighter("Anaconda", 10, 12);
 var improvedFighter = new ImprovedFighter("Tiger", 122, 1);
 
+fight(fighter, improvedFighter, [25, 13, 45]);
+
 // call fight function
