f008f1b589f67046ccc283a18b1f24912b77ffa4
Matvieiev Kyrylo <kyrylo.matvieiev@gmail.com>
2018-05-11 08:17:22 +0300

    Created class ImprovedFighter
    Created class ImprovedFighter and For each class, created two instances - fighter and improvedFighter

--- src/improvedFighter.js
+++ src/improvedFighter.js
@@ -1,1 +1,11 @@
 // Create class ImprovedFighter
+
+class ImprovedFighter extends Fighter {
+    doubleHit(enemy, points) {
+        let doublePoints = new Array(); 
+        points.forEach(element => {
+            doublePoints.push(element * 2);
+        });
+        super.hit(enemy, doublePoints);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 // create two instances
 
+var fighter = new Fighter("Anaconda", 10, 12);
+var improvedFighter = new ImprovedFighter("Tiger", 122, 1);
+
 // call fight function
